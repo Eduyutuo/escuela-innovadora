13730eafefc90f5b46c3eb58b1d01bc01ec086e8
Deploy Tool <deploy@escuela.edu>
2026-03-19 10:01:45 -0500

    Configure Laravel to serve React SPA catch-all

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    $path = public_path('index.html');
+    if (!file_exists($path)) {
+        return "ERROR: Frontend build file (index.html) not found in public directory. Current path: " . $path;
+    }
+    return file_get_contents($path);
+})->where('any', '.*');
